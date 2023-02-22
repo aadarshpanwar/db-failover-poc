50638cad11c0ea9f8ce350ff045d57a0e68affb6
Aadarsh Panwar <aadarshpanwar@creditsaison-in.com>
2023-02-22 23:20:30 +0530

    wrapper read write

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -13,6 +13,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -21,6 +22,7 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @SpringBootApplication
@@ -30,22 +32,9 @@ public class DemoApplication {
 	@Autowired
 	private PersonRepo personRepo;
 
-	@Autowired
-	private DataSource source;
-
-	@Autowired
-	private ApplicationContext applicationContext;
-	@Value("${spring.datasource.url}")
-	private String url;
-	@Value("${spring.datasource.username}")
-	private String username;
-	@Value("${spring.datasource.password}")
-	private String password;
-
-	@Autowired
-	LocalContainerEntityManagerFactoryBean entityManager;
-
 	private RestTemplate template = new RestTemplate();
+
+	private String IP = "3.249.163.205";
 
 	public static void main(String[] args) {
 //		java.security.Security.setProperty("networkaddress.cache.ttl" , "1");
@@ -54,73 +43,33 @@ public class DemoApplication {
 
 	}
 
-	@PostMapping("/person-all")
-	public void add() throws InterruptedException {
-		//return personRepo.save(person);
-		for (int i = 0; i < 1000; i++) {
-
-			try {
-//				InetAddress address = InetAddress.getByName("dbfailover-poc.cluster-cdibd9suphjv.eu-west-1.rds.amazonaws.com");
-//				System.out.println(getCurrentTime() + " lookup success " + address);
-				Person person = new Person();
-				person.setName("name");
-				person.setAge(12);
-				personRepo.save(person);
-				System.out.println(getCurrentTime() + ":saved");
-			} catch (Exception ignore) {
-				System.out.println(getCurrentTime() +",save failed:"+ ignore.getMessage());
-			} finally {
-				Thread.sleep(2000);
-			}
-		}
-	}
 
 	@PostMapping("/person")
 	public Person addOne(@RequestBody Person person) {
 		return personRepo.save(person);
 	}
 
-	private static String getCurrentTime() {
-		DateFormat dateFormat = new SimpleDateFormat("mm:ss");
-		return dateFormat.format(Calendar.getInstance().getTime());
+	@GetMapping("/person-all")
+	@Transactional(readOnly = true)
+	public List<Person> getAll() {
+		return personRepo.findAll();
 	}
 
-	@GetMapping("/soft-evict")
-	public void context() throws InterruptedException {
-		HikariDataSource hikari = (HikariDataSource)source;
-		HikariConfigMXBean config = hikari.getHikariConfigMXBean();
-
-		HikariPool pool = (HikariPool) hikari.getHikariPoolMXBean();
-		hikari.getHikariPoolMXBean().softEvictConnections();
+	@GetMapping("send-find-all/{count}")
+	public void getAll(@PathVariable("count") int count) throws InterruptedException {
+		for(int i = 0;i<count;++i) {
+			try {
+				JsonNode res = template.getForObject("http://"+IP+ ":8080/person-all", JsonNode.class);
+				List<Person> list = personRepo.findAll();
+				System.out.println("Person list size :"+ list.size());
+			}catch(Exception e) {
+				System.out.println("Exception Person list " + e.getMessage());
+			}
+			Thread.sleep(1000);
+		}
 	}
 
-	@GetMapping("pool-stat")
-	public Map<String,Object> getPoolState() {
-		HikariDataSource hikari = (HikariDataSource)source;
-		HikariPool pool = (HikariPool) hikari.getHikariPoolMXBean();
-		Map<String, Object> map = new HashMap<>();
-		map.put("total", pool.getTotalConnections());
-		map.put("idle", pool.getIdleConnections());
-		map.put("active", pool.getActiveConnections());
-		map.put("awaiting", pool.getThreadsAwaitingConnection());
-		return map;
-	}
 
-	public DataSource dataSource(){
-		HikariConfig hikariConfig = new HikariConfig();
-		hikariConfig.setDriverClassName("org.postgresql.Driver");
-		hikariConfig.setJdbcUrl(url);
-		hikariConfig.setUsername(username);
-		hikariConfig.setPassword(password);
-
-		hikariConfig.setMaximumPoolSize(5);
-		hikariConfig.setConnectionTestQuery("SELECT 1");
-
-
-		HikariDataSource dataSource = new HikariDataSource(hikariConfig);
-
-		return dataSource;
-	}
 
 	@GetMapping("/send-req-ec2/{count}")
 	public Object send(@PathVariable("count") int count) throws InterruptedException {
@@ -132,12 +81,12 @@ public class DemoApplication {
 				new HttpEntity<Person>(person, headers);
 		for(int i = 0;i<count;++i) {
 			try {
-				JsonNode res = template.postForObject("http://34.243.29.122:8080/person", request, JsonNode.class);
+				JsonNode res = template.postForObject("http://"+IP+":8080/person", request, JsonNode.class);
 				System.out.println("response :"+ res.toString());
 			}catch(Exception e) {
 				System.out.println("Exception: " + e.getMessage());
 			}
-			Thread.sleep(10000);
+			Thread.sleep(1000);
 		}
 		return null;
 	}
