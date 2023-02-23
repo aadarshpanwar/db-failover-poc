feb367903f2f4240675153b9c6bf918486b2e6bb
Aadarsh Panwar <aadarshpanwar@creditsaison-in.com>
2023-02-23 16:24:56 +0530

    commit

--- src/main/java/com/example/demo/TransactionRoutingConfiguration.java
+++ src/main/java/com/example/demo/TransactionRoutingConfiguration.java
@@ -7,9 +7,11 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.*;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
+import software.amazon.jdbc.ds.AwsWrapperDataSource;
 //import software.amazon.jdbc.ds.AwsWrapperDataSource;
 
 import javax.sql.DataSource;
@@ -32,89 +34,42 @@ public class TransactionRoutingConfiguration {
     @Value("${spring.datasource.password}")
     private String password;
 
-//    @Value("${spring.datasource.hikari.maximumPoolSize}")
-//    private int maxPoolSize;
-//
-//    @Value("${spring.datasource.hikari.minimumIdle}")
-//    private int minimumIdle;
-
     @Bean
-    public DataSource readWriteDataSource() {
-        PGSimpleDataSource ds = new PGSimpleDataSource();
-        ds.setUser(username);
-        ds.setPassword(password);
-        ds.setURL(primaryUrl);
-      //  ds.setDriverClassName("software.aws.rds.jdbc.postgresql.Driver");
-       // ds.setDataSourceClassName("software.aws.rds.jdbc.postgresql.Driver");
-        // Configure AwsWrapperDataSource:
-//        ds.addDataSourceProperty("jdbcProtocol", "jdbc:postgresql:");
-//        ds.addDataSourceProperty("databasePropertyName", "databaseName");
-//        ds.addDataSourceProperty("portPropertyName", "portNumber");
-//        ds.addDataSourceProperty("serverPropertyName", "serverName");
-//
-//        // Specify the driver-specific data source for AwsWrapperDataSource:
-//        ds.addDataSourceProperty("targetDataSourceClassName", "org.postgresql.ds.PGSimpleDataSource");
-//
-//        // Configuring PGSimpleDataSource:
-//        Properties targetDataSourceProps = new Properties();
-//        targetDataSourceProps.setProperty("serverName", "dbfailover-poc.cluster-cdibd9suphjv.eu-west-1.rds.amazonaws.com");
-//        targetDataSourceProps.setProperty("databaseName", "test");
-//        targetDataSourceProps.setProperty("portNumber", "5432");
-//        targetDataSourceProps.setProperty("wrapperPlugins", "failover");
-//
-//        targetDataSourceProps.setProperty("failoverTimeoutMs", "30000");
-//        targetDataSourceProps.setProperty("failoverWriterReconnectIntervalMs", "2000");
-//        targetDataSourceProps.setProperty("failoverReaderConnectTimeoutMs", "10000");
-//        targetDataSourceProps.setProperty("failoverClusterTopologyRefreshRateMs", "2000");
+    public DataSource mysqlDataSource() {
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName("software.amazon.jdbc.Driver");
+//        dataSource.setUrl(primaryUrl);
+//        dataSource.setUsername(username);
+//        dataSource.setPassword(password);
+        HikariDataSource ds = new HikariDataSource();
+        ds.setExceptionOverrideClassName("software.amazon.jdbc.util.HikariCPSQLException");
+            // Configure the connection pool:
+            ds.setUsername(username);
+            ds.setPassword(password);
 
+            // Specify the underlying datasource for HikariCP:
+            ds.setDataSourceClassName(AwsWrapperDataSource.class.getName());
 
-      //  ds.addDataSourceProperty("targetDataSourceProperties", targetDataSourceProps);
-       // return ds;
-        return connectionPoolDataSource(ds);
+            // Configure AwsWrapperDataSource:
+            ds.addDataSourceProperty("jdbcProtocol", "jdbc:postgresql:");
+            ds.addDataSourceProperty("databasePropertyName", "databaseName");
+            ds.addDataSourceProperty("portPropertyName", "portNumber");
+            ds.addDataSourceProperty("serverPropertyName", "serverName");
+            //ds.setJdbcUrl("jdbc:aws-wrapper:postgresql://dbfailover-poc.cluster-cdibd9suphjv.eu-west-1.rds.amazonaws.com:5432/test");
+            // Specify the driver-specific data source for AwsWrapperDataSource:
+            ds.addDataSourceProperty("targetDataSourceClassName", "org.postgresql.ds.PGSimpleDataSource");
+
+            // Configuring PGSimpleDataSource:
+            Properties targetDataSourceProps = new Properties();
+            targetDataSourceProps.setProperty("serverName", "dbfailover-poc.cluster-cdibd9suphjv.eu-west-1.rds.amazonaws.com");
+            targetDataSourceProps.setProperty("databaseName", "test");
+            targetDataSourceProps.setProperty("portNumber", "5432");
+            targetDataSourceProps.setProperty("wrapperPlugins", "failover");
+
+        ds.addDataSourceProperty("targetDataSourceProperties", targetDataSourceProps);
+        return ds;
     }
 
-    @Bean
-    public DataSource readOnlyDataSource() {
-        PGSimpleDataSource ds = new PGSimpleDataSource();
-        ds.setUser(username);
-        ds.setPassword(password);
-        ds.setURL(replicaUrl);
-       // ds.setDataSourceClassName("software.aws.rds.jdbc.postgresql.Driver");
-        //return ds;
-        return connectionPoolDataSource(ds);
-    }
-
-    @Primary
-    @DependsOn({"readWriteDataSource", "readOnlyDataSource"})
-    @Bean
-    public TransactionRoutingDataSource actualDataSource() {
-        TransactionRoutingDataSource routingDataSource = new TransactionRoutingDataSource();
-
-        Map<Object, Object> dataSourceMap = new HashMap<>();
-        dataSourceMap.put(DataSourceType.READ_WRITE, readWriteDataSource());
-        dataSourceMap.put(DataSourceType.READ_ONLY, readOnlyDataSource());
-
-        routingDataSource.setTargetDataSources(dataSourceMap);
-        return routingDataSource;
-    }
-
-    protected HikariConfig hikariConfig(DataSource dataSource) {
-        HikariConfig hikariConfig = new HikariConfig();
-       // hikariConfig.setMaximumPoolSize(maxPoolSize);
-       // hikariConfig.setMinimumIdle(minimumIdle);
-        hikariConfig.setDataSource(dataSource);
-        hikariConfig.setPassword(password);
-
-        hikariConfig.setAutoCommit(false);
-        hikariConfig.setDriverClassName("software.aws.rds.jdbc.postgresql.Driver");
-        return hikariConfig;
-    }
-
-    protected HikariDataSource connectionPoolDataSource(DataSource dataSource) {
-    HikariDataSource source= new HikariDataSource(hikariConfig(dataSource));
-        //source.setDriverClassName("software.aws.rds.jdbc.postgresql.Driver");
-        return source;
-    }
 }
 
  class TransactionRoutingDataSource extends AbstractRoutingDataSource {
