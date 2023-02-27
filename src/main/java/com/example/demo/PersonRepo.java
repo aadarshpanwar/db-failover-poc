8d119393aeaa99562446f1fa9eb3249fbcd79d6e
Aadarsh Panwar <aadarshpanwar@creditsaison-in.com>
2023-02-27 12:28:34 +0530

    add read write 2 db nodes with aws wrapper

--- src/main/java/com/example/demo/PersonRepo.java
+++ src/main/java/com/example/demo/PersonRepo.java
@@ -10,10 +10,12 @@ import java.util.List;
 @Repository
 public interface PersonRepo extends JpaRepository<Person, Integer> {
     @Query(value = "select * from person where first_name = ?1", nativeQuery = true)
-    @Transactional
+    @Transactional(readOnly = true)
     List<Person> findByFirstName(String firstName);
 
     @Query(value = "select * from person where age = ?1", nativeQuery = true)
     @Transactional(readOnly = true)
     List<Person> findByAge(int age);
+
+
 }
