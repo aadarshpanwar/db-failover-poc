1f59e552ac153c7930657fa72a9a00fe18702f7d
Aadarsh Panwar <aadarshpanwar@creditsaison-in.com>
2023-03-02 22:51:41 +0530

    max life time

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -26,83 +26,15 @@ import java.util.List;
 import java.util.Map;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
 
-	@Autowired
-	private PersonRepo personRepo;
-
-	private RestTemplate template = new RestTemplate();
-
-	private String IP = "34.253.182.243";
-
 	public static void main(String[] args) {
-//		java.security.Security.setProperty("networkaddress.cache.ttl" , "1");
-//		java.security.Security.setProperty("networkaddress.cache.negative.ttl" , "3");
 		SpringApplication.run(DemoApplication.class, args);
 
 	}
 
 
-	@PostMapping("/person")
-	public Person addOne(@RequestBody Person person) {
-		return personRepo.save(person);
-	}
-
-	@GetMapping("/person-all")
-	@Transactional(readOnly = true)
-	public List<Person> getAll() {
-		return personRepo.findAll();
-	}
-
-	@GetMapping("/send-local/{count}")
-	public void sendPostLocal(@PathVariable("count") int count) throws InterruptedException {
-		for(int i = 0;i<count;++i) {
-			try {
-				Person person = new Person();
-				person.setFirstName("ram");
-				person.setAge(12);
-				personRepo.save(person);
-				System.out.println("saved :"+ person.getId());
-			}catch(Exception e) {
-				System.out.println("Exception Person list " + e.getMessage());
-			}
-			Thread.sleep(1000);
-		}
-	}
-	@GetMapping("send-find-all/{count}")
-	public void getAll(@PathVariable("count") int count) throws InterruptedException {
-		for(int i = 0;i<count;++i) {
-			try {
-				JsonNode res = template.getForObject("http://"+IP+ ":8080/person-all", JsonNode.class);
-				List<Person> list = personRepo.findAll();
-				System.out.println("Person list size :"+ list.size());
-			}catch(Exception e) {
-				System.out.println("Exception Person list " + e.getMessage());
-			}
-			Thread.sleep(1000);
-		}
-	}
 
 
 
-	@GetMapping("/send-req-ec2/{count}")
-	public Object send(@PathVariable("count") int count) throws InterruptedException {
-		 Person person = new Person();
-		 person.setFirstName("ram");
-		 person.setAge(12);
-		HttpHeaders headers = new HttpHeaders();
-		HttpEntity<Person> request =
-				new HttpEntity<Person>(person, headers);
-		for(int i = 0;i<count;++i) {
-			try {
-				JsonNode res = template.postForObject("http://"+IP+":8080/person", request, JsonNode.class);
-				System.out.println("response :"+ res.toString());
-			}catch(Exception e) {
-				System.out.println("Exception: " + e.getMessage());
-			}
-			Thread.sleep(1000);
-		}
-		return null;
-	}
 }
--- src/main/java/com/example/demo/TransactionRoutingConfiguration.java
+++ src/main/java/com/example/demo/TransactionRoutingConfiguration.java
@@ -1,70 +1,70 @@
-package com.example.demo;
-
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import software.amazon.jdbc.ds.AwsWrapperDataSource;
-
-import javax.sql.DataSource;
-import java.util.Properties;
-
-@Configuration
-public class TransactionRoutingConfiguration {
-
-    @Value("${jdbc.url.primary-endpoint}")
-    private String primaryEndpoint;
-
-    @Value("${spring.datasource.username}")
-    private String username;
-
-    @Value("${spring.datasource.password}")
-    private String password;
-
-    @Value("${jdbc.url.primary-port}")
-    private String port;
-
-    @Value("${jdbc.url.primary-databasename}")
-    private String dbName;
-
-    @Value(("${jdbc.url.primary-schema}"))
-    private String schema;
-    private int maxPoolSize = 5;
-
-    private int minimumIdle = 1;
-
-    @Bean
-    public DataSource dataSource() {
-        return new HikariDataSource(hikariConfig(primaryEndpoint));
-    }
-    protected HikariConfig hikariConfig(String primaryEndpoint) {
-        HikariConfig hikariConfig = new HikariConfig();
-        //Hikari pool specific config
-        hikariConfig.setUsername(username);
-        hikariConfig.setPassword(password);
-        hikariConfig.setAutoCommit(false);
-        //Require for enable Aws wrapper support
-        hikariConfig.setDataSourceClassName(AwsWrapperDataSource.class.getName());
-        hikariConfig.addDataSourceProperty("jdbcProtocol", "jdbc:postgresql:");
-        hikariConfig.addDataSourceProperty("databasePropertyName", "databaseName");
-        hikariConfig.addDataSourceProperty("portPropertyName", "portNumber");
-        hikariConfig.addDataSourceProperty("serverPropertyName", "serverName");
-        hikariConfig.addDataSourceProperty("targetDataSourceClassName", "org.postgresql.ds.PGSimpleDataSource");
-
-        Properties targetDataSourceProps = new Properties();
-        //Db specific config
-        targetDataSourceProps.setProperty("currentSchema", schema);
-        targetDataSourceProps.setProperty("serverName", primaryEndpoint);
-        targetDataSourceProps.setProperty("databaseName", dbName);
-        targetDataSourceProps.setProperty("portNumber", port);
-        //failover plugin specific config
-        targetDataSourceProps.setProperty("wrapperPlugins", "failover");
-        targetDataSourceProps.setProperty("failoverTimeoutMs", "30000");
-        targetDataSourceProps.setProperty("failoverWriterReconnectIntervalMs", "2000");
-        targetDataSourceProps.setProperty("failoverReaderConnectTimeoutMs", "10000");
-        targetDataSourceProps.setProperty("failoverClusterTopologyRefreshRateMs", "2000");
-        hikariConfig.addDataSourceProperty("targetDataSourceProperties", targetDataSourceProps);
-        return hikariConfig;
-    }
-}
+//package com.example.demo;
+//
+//import com.zaxxer.hikari.HikariConfig;
+//import com.zaxxer.hikari.HikariDataSource;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import software.amazon.jdbc.ds.AwsWrapperDataSource;
+//
+//import javax.sql.DataSource;
+//import java.util.Properties;
+//
+//@Configuration
+//public class TransactionRoutingConfiguration {
+//
+//    @Value("${jdbc.url.primary-endpoint}")
+//    private String primaryEndpoint;
+//
+//    @Value("${spring.datasource.username}")
+//    private String username;
+//
+//    @Value("${spring.datasource.password}")
+//    private String password;
+//
+//    @Value("${jdbc.url.primary-port}")
+//    private String port;
+//
+//    @Value("${jdbc.url.primary-databasename}")
+//    private String dbName;
+//
+//    @Value(("${jdbc.url.primary-schema}"))
+//    private String schema;
+//    private int maxPoolSize = 5;
+//
+//    private int minimumIdle = 1;
+//
+//    @Bean
+//    public DataSource dataSource() {
+//        return new HikariDataSource(hikariConfig(primaryEndpoint));
+//    }
+//    protected HikariConfig hikariConfig(String primaryEndpoint) {
+//        HikariConfig hikariConfig = new HikariConfig();
+//        //Hikari pool specific config
+//        hikariConfig.setUsername(username);
+//        hikariConfig.setPassword(password);
+//        hikariConfig.setAutoCommit(false);
+//        //Require for enable Aws wrapper support
+//        hikariConfig.setDataSourceClassName(AwsWrapperDataSource.class.getName());
+//        hikariConfig.addDataSourceProperty("jdbcProtocol", "jdbc:postgresql:");
+//        hikariConfig.addDataSourceProperty("databasePropertyName", "databaseName");
+//        hikariConfig.addDataSourceProperty("portPropertyName", "portNumber");
+//        hikariConfig.addDataSourceProperty("serverPropertyName", "serverName");
+//        hikariConfig.addDataSourceProperty("targetDataSourceClassName", "org.postgresql.ds.PGSimpleDataSource");
+//
+//        Properties targetDataSourceProps = new Properties();
+//        //Db specific config
+//        targetDataSourceProps.setProperty("currentSchema", schema);
+//        targetDataSourceProps.setProperty("serverName", primaryEndpoint);
+//        targetDataSourceProps.setProperty("databaseName", dbName);
+//        targetDataSourceProps.setProperty("portNumber", port);
+//        //failover plugin specific config
+//        targetDataSourceProps.setProperty("wrapperPlugins", "failover");
+//        targetDataSourceProps.setProperty("failoverTimeoutMs", "30000");
+//        targetDataSourceProps.setProperty("failoverWriterReconnectIntervalMs", "2000");
+//        targetDataSourceProps.setProperty("failoverReaderConnectTimeoutMs", "10000");
+//        targetDataSourceProps.setProperty("failoverClusterTopologyRefreshRateMs", "2000");
+//        hikariConfig.addDataSourceProperty("targetDataSourceProperties", targetDataSourceProps);
+//        return hikariConfig;
+//    }
+//}
